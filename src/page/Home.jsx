5ce79366d7cca3d69ee99a662a8b598e34783cc6
Petrus <110820003+PetrusGeyu@users.noreply.github.com>
2024-04-24 05:17:05 +0700

    gass

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import '../App.css'
+import { useNavigate } from 'react-router';
 
 const Home = () => {
   const userData = JSON.parse(localStorage.getItem('userData'));
   console.log(userData);
 
+    const navigate = useNavigate();
+  
+    const handleLogout = () => {
+      // Hapus token dari localStorage
+      localStorage.removeItem('tokens');
+      localStorage.removeItem('userData'); // Opsional: Hapus data pengguna lainnya jika ada
+  
+      // Redirect ke halaman login atau halaman lainnya
+      navigate('/login');
+    }
   return (
    <>
    <nav>
     <h1>FaceGram</h1>
 
-    <h4>{userData.username}</h4>
+    <div className='menu'>
+      <h4>{userData.username}</h4>
+      <button className='logout' onClick={handleLogout}>Logout</button>
+    </div>
    </nav>
    </>
   )
